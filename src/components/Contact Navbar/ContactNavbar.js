588247ca6f9db597db4a953f400f97e0aaa3c03f
bawa-yatin <Yatin.bawa@mindbowser.com>
2022-07-22 00:57:26 +0530

    Completed Frontend part of Stripe Assignment along with Responsiveness

--- src/components/Contact Navbar/ContactNavbar.js
+++ src/components/Contact Navbar/ContactNavbar.js
@@ -6,14 +6,14 @@ const ContactNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" className="py-1">
       <Container className="text-margin-style">
-        <Navbar.Brand className="navbar-text-size">
+        <Navbar.Brand className="navbar-text-size fw-bold">
           <Navbar.Text>Mon-Thu: </Navbar.Text>
           9:00 AM - 5:30 PM
         </Navbar.Brand>
         <Navbar.Toggle />
 
         <Navbar.Collapse className="justify-content-end navbar-text-size">
-          <Navbar.Text className="text-white">
+          <Navbar.Text className="text-white fw-bold">
             Call Us: (00) 1234 5678
           </Navbar.Text>
         </Navbar.Collapse>
--- src/components/Product Info/ProductInfo.js
+++ src/components/Product Info/ProductInfo.js
@@ -1,6 +1,7 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button";
 import "./ProductInfo.css";
 import React from "react";
 import slider_img_1 from "../../assets/images/carousel_img_1.png";
@@ -12,13 +13,26 @@ const ProductInfo = () => {
   return (
     <React.Fragment>
       <Row className="mx-0 bottom-margin">
-        <Col xl={7} lg={6} sm={12} className="product-desc d-none d-lg-block">
-          <div className="my-5 mx-2">
-            <span style={{ opacity: "0.5", fontSize: "11px" }}>
+        <Col
+          xl={{ span: 7, order: 1 }}
+          lg={{ span: 6, order: 1 }}
+          md={{ span: 12, order: 2 }}
+          sm={{ span: 12, order: 2 }}
+          xs={{ span: 12, order: 2 }}
+          className="product-desc"
+        >
+          <div className="mx-2 mx-xl-0 content-spacing">
+            <p className="fw-bold product-border d-xl-none d-lg-none d-md-block d-sm-block d-xs-block">
+              About Product
+            </p>
+            <span
+              className="d-xl-block d-lg-block d-md-none d-sm-none d-none"
+              style={{ opacity: "0.5", fontSize: "11px" }}
+            >
               APPLE PRODUCTS
             </span>
             <h4 className="mt-4 mb-3">New Apple AirPods (3rd Generation)</h4>
-            <p style={{ fontSize: "12px" }}>It's magic, remastered!</p>
+            <p style={{ fontSize: "14px" }}>It's magic, remastered!</p>
             <ul className="list-space">
               <li>
                 Spatial audio with dynamic head tracking places sound all around
@@ -41,14 +55,21 @@ const ProductInfo = () => {
           </div>
         </Col>
 
-        <Col xl={5} lg={6} sm={12} className="px-0 img slider">
+        <Col
+          xl={{ span: 5, order: 2 }}
+          lg={{ span: 6, order: 2 }}
+          md={{ span: 12, order: 1 }}
+          sm={{ span: 12, order: 1 }}
+          xs={{ span: 12, order: 1 }}
+          className="px-0 img slider"
+        >
           <Carousel controls={false} interval={1500}>
             <Carousel.Item>
               <img
                 className="d-block w-100 img-style"
                 src={slider_img_1}
                 alt="First Image"
-                height="415"
+                height="470"
               />
             </Carousel.Item>
             <Carousel.Item>
@@ -56,7 +77,7 @@ const ProductInfo = () => {
                 className="d-block w-100 img-style"
                 src={slider_img_2}
                 alt="Second Image"
-                height="415"
+                height="470"
               />
             </Carousel.Item>
             <Carousel.Item>
@@ -64,7 +85,7 @@ const ProductInfo = () => {
                 className="d-block w-100 img-style"
                 src={slider_img_3}
                 alt="Third Image"
-                height="415"
+                height="470"
               />
             </Carousel.Item>
 
@@ -73,12 +94,27 @@ const ProductInfo = () => {
                 className="d-block w-100 img-style"
                 src={slider_img_4}
                 alt="Fourth Image"
-                height="415"
+                height="470"
               />
             </Carousel.Item>
           </Carousel>
         </Col>
       </Row>
+      <div className="d-xl-none d-lg-none d-md-block d-sm-block d-xs-block price-card">
+        <div className="mt-sm-3 mx-sm-4 py-3 mx-3">
+          <p className="d-sm-inline-block d-inline-block mt-sm-2 my-2">
+            Price <b>&#8377;299.00</b>
+          </p>
+          <Button
+            variant="primary"
+            size="sm"
+            className="purchase-btn-style fw-bold d-sm-inline-block"
+            style={{ float: "right" }}
+          >
+            Buy Now
+          </Button>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
